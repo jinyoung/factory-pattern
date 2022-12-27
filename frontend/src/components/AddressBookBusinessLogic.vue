09c025c3d8c424b836da2ce7fdc1f26a74b8025f
Jinyoung Jang <jyjang@uengine.org>
2022-12-27 15:24:32 +0900

    commit

---
+++ frontend/src/components/AddressBookBusinessLogic.vue
@@ -0,0 +1,217 @@
+<template>
+
+    <v-card style="width:450px;" outlined>
+        <template slot="progress">
+            <v-progress-linear
+                    color="deep-purple"
+                    height="10"
+                    indeterminate
+            ></v-progress-linear>
+        </template>
+
+        <v-card-title v-if="value._links">
+            AddressBookBusinessLogic # {{value._links.self.href.split("/")[value._links.self.href.split("/").length - 1]}}
+        </v-card-title >
+        <v-card-title v-else>
+            AddressBookBusinessLogic
+        </v-card-title >
+
+        <v-card-text>
+            <Database offline label="Database" v-model="value.database" :editMode="editMode" @change="change"/>
+        </v-card-text>
+
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="edit"
+                    v-if="!editMode"
+            >
+                Edit
+            </v-btn>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="save"
+                    v-else
+            >
+                Save
+            </v-btn>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="remove"
+                    v-if="!editMode"
+            >
+                Delete
+            </v-btn>
+            <v-btn
+                    color="deep-purple lighten-2"
+                    text
+                    @click="editMode = false"
+                    v-if="editMode && !isNew"
+            >
+                Cancel
+            </v-btn>
+        </v-card-actions>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+        </v-card-actions>
+
+        <v-snackbar
+                v-model="snackbar.status"
+                :top="true"
+                :timeout="snackbar.timeout"
+                color="error"
+        >
+            {{ snackbar.text }}
+            <v-btn dark text @click="snackbar.status = false">
+                Close
+            </v-btn>
+        </v-snackbar>
+    </v-card>
+    <div style="margin: 0 -15px 0 -15px;">
+        <v-card-title>
+            
+        </v-card-title>
+
+        <v-card-text>
+        </v-card-text>
+
+        <v-card-actions v-if="inList">
+            <slot name="actions"></slot>
+        </v-card-actions>
+    </div>
+</template>
+
+<script>
+    
+    const axios = require('axios').default;
+    
+
+
+    export default {
+        name: 'AddressBookBusinessLogic',
+        components:{
+        },
+        props: {
+            value: [Object, String, Number, Boolean, Array],
+            editMode: Boolean,
+            isNew: Boolean,
+            offline: Boolean,
+            inList: Boolean,
+            label: String,
+        },
+        data: () => ({
+        
+            snackbar: {
+                status: false,
+                timeout: 5000,
+                text: ''
+            },
+        
+        }),
+        async created() {
+        },
+        watch: {
+            
+            value(val) {
+                this.$emit('input', val);
+            },
+            newValue(val) {
+                this.$emit('input', val);
+            },
+            
+        },
+
+        methods: {
+            edit() {
+                this.editMode = true;
+            },
+            
+            async save(){
+                try {
+                    var temp = null;
+
+                    if(!this.offline) {
+                        if(this.isNew) {
+                            temp = await axios.post(axios.fixUrl('/addressbookbusinesslogics'), this.value)
+                        } else {
+                            temp = await axios.put(axios.fixUrl(this.value._links.self.href), this.value)
+                        }
+                    }
+
+                    if(this.value!=null) {
+                        for(var k in temp.data) this.value[k]=temp.data[k];
+                    } else {
+                        this.value = temp.data;
+                    }
+
+                    this.editMode = false;
+                    this.$emit('input', this.value);
+
+                    if (this.isNew) {
+                        this.$emit('add', this.value);
+                    } else {
+                        this.$emit('edit', this.value);
+                    }
+
+                    location.reload()
+
+                } catch(e) {
+                    this.snackbar.status = true
+                    if(e.response && e.response.data.message) {
+                        this.snackbar.text = e.response.data.message
+                    } else {
+                        this.snackbar.text = e
+                    }
+                }
+                
+            },
+            async remove(){
+                try {
+                    if (!this.offline) {
+                        await axios.delete(axios.fixUrl(this.value._links.self.href))
+                    }
+
+                    this.editMode = false;
+                    this.isDeleted = true;
+
+                    this.$emit('input', this.value);
+                    this.$emit('delete', this.value);
+
+                } catch(e) {
+                    this.snackbar.status = true
+                    if(e.response && e.response.data.message) {
+                        this.snackbar.text = e.response.data.message
+                    } else {
+                        this.snackbar.text = e
+                    }
+                }
+            },
+            async add() {
+                this.editMode = false;
+                this.$emit('input', this.value);
+
+                if(this.isNew){
+                    this.$emit('add', this.value);
+                } else {
+                    this.$emit('edit', this.value);
+                }
+            },
+            async remove(){
+                this.editMode = false;
+                this.isDeleted = true;
+
+                this.$emit('input', this.value);
+                this.$emit('delete', this.value);
+            },
+            change(){
+                this.$emit('change', this.value);
+            },
+            
+        }
+    }
+</script>
+
